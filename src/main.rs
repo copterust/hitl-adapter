b09dbf72aeb418cbdd289cc3c02cd23d58acf976
Alexander Zhuravlev <123368+bofh@users.noreply.github.com>
2019-11-05 18:23:20 +0100

    Send servo control packet

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::mem;
 use std::net::UdpSocket;
-use std::os::raw::c_double;
+use std::os::raw::{c_float, c_double};
 
 #[repr(C)]
 #[derive(Debug)]
@@ -19,12 +19,21 @@ pub struct FDMPacket {
   position: [c_double; 3],
 }
 
+type ServoPacket = [c_float; 16];
+
 fn main() {
     let socket_in = UdpSocket::bind("0.0.0.0:9003").expect("couldn't bind receiving socket");
     let socket_out = UdpSocket::bind("0.0.0.0:0").expect("couldn't bind sending socket");
 
-    let buf_out = [127; 16];
     let mut buf_in = [0; 136];
+
+    let mut command: ServoPacket = [0.0; 16];
+    command[0] = 1.0;
+    command[1] = 1.0;
+    command[2] = 1.0;
+    command[3] = 1.0;
+
+    let buf_out: [u8; 64] = unsafe { mem::transmute(command) };
 
     loop {
         socket_out.send_to(&buf_out, "127.0.0.1:9002").expect("couldn't send data");
